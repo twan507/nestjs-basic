fe9f1b9379fa0031c2459732b4d688c94876bed9
twan507 <tuanbuianh507@gmail.com>
2024-01-22 16:27:46 +0700

    22.4

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
-require('dotenv').config()
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule,);
@@ -11,6 +11,8 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
+
+  app.useGlobalPipes(new ValidationPipe())
 
   await app.listen(configService.get<string>('PORT'));
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,15 @@
+import { IsEmail, IsNotEmpty } from "class-validator"
+
 export class CreateUserDto {
+
+    @IsEmail({},{message: "Email không đúng định dạng"})
+    @IsNotEmpty({message: "Email không được để trống"})
     email: string
+
+    @IsNotEmpty({message: "Pasword không được để trống"})
     password: string
+
+
     name: string
     address: string
 }
