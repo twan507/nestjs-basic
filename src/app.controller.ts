869228a80397e08153e45608f217b586b258fab6
twan507 <tuanbuianh507@gmail.com>
2024-01-22 11:57:22 +0700

    19

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,18 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService
+  ) { }
 
   @Get()
   @Render("home")
   handleHomePage() {
-    const message =  this.appService.getHello();
+    const message = this.appService.getHello();
     return {
       message: message
     }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // MongooseModule.forRoot('mongodb+srv://tuanbuianh507:DL7SfT6XVBgXYcql@nestjs.xzwfjyy.mongodb.net/'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
