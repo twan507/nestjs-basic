36ddca701ba4086f1b157851c3090d1e384793df
twan507 <tuanbuianh507@gmail.com>
2024-01-23 18:10:26 +0700

    38

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -9,12 +10,9 @@ export class AppController {
     private configService: ConfigService
   ) { }
 
-  @Get()
-  @Render("home")
-  handleHomePage() {
-    const message = this.appService.getHello();
-    return {
-      message: message
-    }
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  handleLogin(@Request() req ){
+    return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,16 +7,14 @@ export class AuthService {
 
     //username và password là 2 tham số mà thư viện passport ném về
     async validateUser(username: string, pass: string): Promise<any> {
-
         const user = await this.usersService.findOneByUsername(username);
-
         if (user) {
             const isValid = this.usersService.isValidPassword(pass, user.password)
             if (isValid === true) {
                 return user
             }
         }
-        
+
         return null;
     }
 }
