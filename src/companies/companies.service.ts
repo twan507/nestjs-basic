3cc23cb632be5df6da992c85ced9f3ec4b33aab1
“twan” <tuanbuianh507@gmail.com>
2024-01-24 13:32:36 +0700

    54

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Company, CompanyDocument } from './schemas/company.schemas';
 
 @Injectable()
 export class CompaniesService {
+
+  constructor(
+    @InjectModel(Company.name)
+    private companyModel: SoftDeleteModel<CompanyDocument>
+  ) { }
+
   create(createCompanyDto: CreateCompanyDto) {
-    return 'This action adds a new company';
+    return this.companyModel.create({ ...CreateCompanyDto })
   }
 
   findAll() {
